6f486723c0c40c05e59fc716a39932cfef9ef2da
Favor Vincent <vfavorm77@gmail.com>
2024-07-03 16:50:45 +0300

    flask crud lab complete

--- server/app.py
+++ server/app.py
@@ -47,6 +47,22 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        
+        plant.is_in_stock = data['is_in_stock']
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response(" ", 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
